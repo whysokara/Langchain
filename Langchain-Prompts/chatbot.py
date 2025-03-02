766f71fc72721bb85e561da8e8297b36045aef58
Kara <kara_himanshu@yahoo.com>
2025-03-02 14:12:06 +0530

    integrated messages in chatbot

--- Langchain-Prompts/chatbot.py
+++ Langchain-Prompts/chatbot.py
@@ -1,21 +1,24 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage, AIMessage, SystemMessage
 
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model='gemini-1.5-pro', temperature=0.4)
 
-chat_history =  []
+chat_history = [
+    SystemMessage(content='You are highly aware and confident assistant')
+]
 print("Welcome to bot K")
-while True:
 
-    
+while True:
     user_input = input("You: ")
-    chat_history.append(user_input)
+    chat_history.append(HumanMessage(content=user_input))
+
     if user_input == 'exit':
         break
     result = model.invoke(chat_history)
-    chat_history.append(result.content)
+    chat_history.append(AIMessage(content=result.content))
     print("AI: ", result.content)
 
 print(chat_history)
