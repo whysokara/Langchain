f96debcc931bf20ab8c679e6f3f7fc8bb7f0769e
Kara <kara_himanshu@yahoo.com>
2025-03-02 14:05:50 +0530

    types of messages in llm/langchain

---
+++ Langchain-Prompts/messages.py
@@ -0,0 +1,21 @@
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model='gemini-1.5-pro', temperature=0.4)
+
+messages = [
+    SystemMessage(content='You are ahelpful assistant'),
+    # HumanMessage(content='Tell me everything about human micro expressions..')
+    HumanMessage(content='Tell me capital of India')
+]
+
+result = model.invoke(messages)
+
+messages.append(AIMessage(content=result.content))
+print(messages)
+
+
+
